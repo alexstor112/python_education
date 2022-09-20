1ba5599c1913b60756454607cff20c1c56871fbc
Alexei Storozhev <65788722+strox812@users.noreply.github.com>
2022-09-20 20:19:55 +0800

    версия 1

--- 1.py
+++ 1.py
@@ -1,12 +1,23 @@
+from itertools import count
 import random
 
 number = random.randint(1, 100)
 user_number = None
+count = 0
+levels = {1: 10, 2: 5, 3: 3}
+level = int(input('Выберите уровень сложности: '))
+max_count = levels[level]
+
 while number != user_number:
+    count += 1
+    if count > max_count:
+        print('Вы проиграли')
+        break
+    print(f'Попытка № {count}')
     user_number = int(input('Введите число: '))
     if number < user_number:
         print('Ваше число больше загаданного')
     elif number > user_number:
         print('Ваше число меньше загаданного')
-
-print('Победа!')
+else:
+    print('Победа!')
